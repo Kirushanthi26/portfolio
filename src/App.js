5f04f6bc75079fb279bd17fa95ba5878746757a5
kirushanthi <kirushanthikiru26@gmail.com>
2021-09-12 14:08:17 +0530

    setup the Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello kirushanthi </h1>
+      <div className="md:container md:mx-auto">
+          <h1 className="">kirushanthi Letchumanan</h1>
+      </div>
     </div>
   );
 }
